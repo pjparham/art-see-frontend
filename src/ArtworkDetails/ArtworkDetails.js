fd6caa426f88431d7c4f28096c662653342124ac
Parker Parham <parkerparham@gmail.com>
2022-11-15 19:33:49 -0500

    Added edit button to ArtDetails

--- src/ArtworkDetails/ArtworkDetails.js
+++ src/ArtworkDetails/ArtworkDetails.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Comments from '../Comments'
 import { ArtContainer } from './ArtworkDetailsComponents'
 import { useParams } from 'react-router-dom'
 
 export default function ArtworkDetails({ artworks }) {
+  const [edit, setEdit] = useState(false)
     const params = useParams()
 
     const artwork = artworks.find(art => art.id === parseInt(params.id))
@@ -12,13 +13,14 @@ export default function ArtworkDetails({ artworks }) {
   if (!artwork) return <h1>Loading...</h1>
   return (
     <>
-      <ArtContainer>
+      <span>Does something look off?  </span><button onClick={()=>setEdit(!edit)}>Edit info</button>
+    {edit ? null :  <><ArtContainer>
           <h1>{artwork.name}</h1>
             <img src={artwork.image_url} alt="artwork iamge"></img>
             <p>{artwork.artist} | {artwork.year} | {artwork.medium} | {artwork.inch_height} in. x {artwork.inch_width} in.</p>
             {/* <Link to={`/artworks/${artwork.id}`}>{artwork.reviews.length} {artwork.reviews.length === 1 ? "review" : "reviews"}</Link> */}
       </ArtContainer>
-      <Comments comments={artwork.reviews} artwork={artwork} />
+      <Comments comments={artwork.reviews} artwork={artwork} /></>}
     </>
   )
 }
